a3c243af3e723b26cd7b60a6e21b0a2b6b97ef9c
vinayak joshi <vinayakjoshi@vinayaks-MacBook-Air.local>
2026-01-17 17:40:31 +0530

    DAY 1: domain enums defined and locked

---
+++ src/domain/enums/index.ts
@@ -0,0 +1,7 @@
+export { UserRole } from './userRole';
+export { UserStatus } from './userStatus';
+export { TrustTier } from './trustTier';
+export { Language } from './language';
+export { ItemState } from './itemState';
+export { BookingState } from './bookingState';
+export { DamageType } from './damageType';
